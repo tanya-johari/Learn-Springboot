ecfc4bb22bea8fcc0df12012b4c1193bf6c25bab
tanyajohari <johtanya11@gmail.com>
2024-04-22 13:09:40 +0530

    handle http GET request

---
+++ src/main/java/com/example/mobilt/app/ws/ui/controller/UserControlller.java
@@ -0,0 +1,42 @@
+package com.example.mobilt.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users") // http://localhost:8080/users
+public class UserControlller {
+	
+	@GetMapping()
+	public String getUser(@RequestParam(value="page", defaultValue="1") int page, 
+			@RequestParam(value="limit", defaultValue="50") int limit, 
+			@RequestParam(value="sort", defaultValue="desc", required=false) String sort) {
+		return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
+	}
+	
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "get user was called with userId: " + userId;
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "create user was called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user was called";
+	}
+}
